00d2c1e65cd626e6e38c3df8a3bc7dae413a8ebe
Heinrich Weil <hl.weil@t-online.de>
2021-02-03 17:15:51 +0100

    add Optional list appendage to UpdateAllAppendLists

--- src/ISADotnet/API/Update.fs
+++ src/ISADotnet/API/Update.fs
@@ -39,99 +39,112 @@ module Update =
                     Some(v.GetValue(a, [| |]))
                 else 
                     None
-
-    /// if object 'a is a record type, returns a tuple list of fieldNames and fieldValues.
-    let private getFieldNameValues (objValue:'a) =
-        objValue.GetType().GetProperties() |> Array.map (fun n -> n.Name, n.GetValue(objValue) )
+    
+    let private makeOptionValue typey v isSome =
+        let optionType = typeof<unit option>.GetGenericTypeDefinition().MakeGenericType([|typey|])
+        let cases = FSharp.Reflection.FSharpType.GetUnionCases(optionType)
+        let cases = cases |> Array.partition (fun x -> x.Name = "Some")
+        let someCase = fst cases |> Array.exactlyOne
+        let noneCase = snd cases |> Array.exactlyOne
+        let relevantCase, args =
+            match isSome with
+            | true -> someCase, [| v |]
+            | false -> noneCase, [| |]
+        FSharp.Reflection.FSharpValue.MakeUnion(relevantCase, args)
 
     /// updates oldRT with newRT by replacing all values, but appending all lists.
     ///
     /// newRTList@oldRTList
-    let private updateAppend (oldRT:'a) (newRT:'a) = 
-        let newMap  = getFieldNameValues newRT |> Map.ofArray
-        let oldType = getFieldNameValues oldRT
-        let fields =
-            /// map over old recordType
-            oldType |> Array.map (fun (fieldName, fieldValue) ->
-                /// match all field Values and try to cast them to types.
-                match fieldValue with
-                /// Strings are IEnumerable Chars but should not be appenden. So these have to be handled first.
-                | :? String ->
-                    newMap.[fieldName]
-                /// Match all IEnumarables, like list, array, seq. These should be appended.
-                | :? System.Collections.IEnumerable -> 
-                    let oldSeq = fieldValue
-                    let newSeq = newMap.[fieldName]
-                    let fieldT = fieldValue.GetType()
-                    /// Maps are IEnumerables but are not easily to append. TODO(?)
-                    let isMap =
-                        let genericMap = typeof<Map<_,_>>
-                        fieldT.Name = genericMap.Name
-                    /// t is the type of the IEnumerable elements.
-                    let t =
-                        /// element types are accessed differently for (list, seq) and Array. 
-                        if fieldT.IsArray then fieldT.GetElementType() else
-                            fieldValue.GetType().GetGenericArguments() |> Array.head
-                    /// This function accesses the append method of the list/array module and applies it accordingly to the element type.
-                    let appendGenericListsByType l1 l2 (t:Type) =
-                        // https://stackoverflow.com/questions/41253131/how-to-create-an-empty-list-of-a-specific-runtime-type
-                        System.Reflection.Assembly
-                            .GetAssembly(typeof<_ list>)
-                            .GetType(if fieldT.IsArray then "Microsoft.FSharp.Collections.ArrayModule" else "Microsoft.FSharp.Collections.ListModule")
-                            .GetMethod("Append")
-                            .MakeGenericMethod(t)
-                            .Invoke(null, [|l1;l2|])
-                    /// If the IEnumerable is a map then we just replace with the new entry.
-                    if isMap then 
-                        newMap.[fieldName]
-                    else
-                        let r = 
-                            appendGenericListsByType newSeq oldSeq t
-                        r |> box
-                /// All others do not need to be appended and can be replaced.
-                | others -> 
-                    newMap.[fieldName]
-            )
-        FSharpValue.MakeRecord(typeof<'a>, fields) :?> 'a
+    let rec private updateAppend (oldVal: obj) (newVal:obj) = 
+        
+        /// match all field Values and try to cast them to types.
+        match oldVal with
+        /// Strings are IEnumerable Chars but should not be appenden. So these have to be handled first.
+        | :? String ->
+            newVal
+        | SomeObj(oldInternal) ->
+            match oldInternal with
+            | :? String -> 
+                newVal
+            | :? System.Collections.IEnumerable ->
+                let newOpt = newVal
+                match newOpt with
+                | SomeObj newInternal ->
+                    updateAppend oldInternal newInternal
+                    |> fun v -> makeOptionValue (oldInternal.GetType()) v true
+                | _ -> oldVal
+            | _ -> 
+                newVal
+        /// Match all IEnumarables, like list, array, seq. These should be appended.
+        | :? System.Collections.IEnumerable -> 
+            let oldSeq = oldVal
+            let newSeq = newVal
+            let fieldT = oldVal.GetType()
+            /// Maps are IEnumerables but are not easily to append. TODO(?)
+            let isMap =
+                let genericMap = typeof<Map<_,_>>
+                fieldT.Name = genericMap.Name
+            /// t is the type of the IEnumerable elements.
+            let t =
+                /// element types are accessed differently for (list, seq) and Array. 
+                if fieldT.IsArray then fieldT.GetElementType() else
+                    oldVal.GetType().GetGenericArguments() |> Array.head
+            /// This function accesses the append method of the list/array module and applies it accordingly to the element type.
+            let appendGenericListsByType l1 l2 (t:Type) =
+                // https://stackoverflow.com/questions/41253131/how-to-create-an-empty-list-of-a-specific-runtime-type
+                System.Reflection.Assembly
+                    .GetAssembly(typeof<_ list>)
+                    .GetType(if fieldT.IsArray then "Microsoft.FSharp.Collections.ArrayModule" else "Microsoft.FSharp.Collections.ListModule")
+                    .GetMethod("Append")
+                    .MakeGenericMethod(t)
+                    .Invoke(null, [|l1;l2|])
+            /// If the IEnumerable is a map then we just replace with the new entry.
+            if isMap then 
+                newVal
+            else
+                let r = 
+                    appendGenericListsByType oldSeq newSeq t
+                r |> box
+        /// All others do not need to be appended and can be replaced.
+        | others -> 
+            newVal
+
 
     /// updates oldRT with newRT by replacing all values, but only if the new value is not empty.
-    let private updateOnlyByExisting (oldRT:'a) (newRT:'a) =
-        let newMap  = getFieldNameValues newRT |> Map.ofArray
-        let oldType = getFieldNameValues oldRT
-        let fields =
-            /// map over oldRT 
-            oldType |> Array.map (fun (fieldName, fieldValue) ->
-                /// try to cast values to types to check for isEmpty according to type.
-                match fieldValue with 
-                /// Check if newValue isNull = isEmpty
-                | _ when newMap.[fieldName] = null ->
-                    fieldValue
-                /// Handle OptionTypes
-                // https://stackoverflow.com/questions/6289761/how-to-downcast-from-obj-to-optionobj
-                /// Check of value is option, then check if new value isNone = isEmpty
-                | SomeObj(opt) ->
-                    let newOpt = newMap.[fieldName]
-                    match newOpt with
-                    | SomeObj n ->
-                        newMap.[fieldName]
-                    | _ -> 
-                        fieldValue
-                /// Check if value is string, then check if new value is "" = isEmpty
-                | :? String ->
-                    let newStr = newMap.[fieldName]
-                    if string newStr = "" then fieldValue else newStr
-                /// https://stackoverflow.com/questions/47280544/determine-if-any-kind-of-list-sequence-array-or-ienumerable-is-empty
-                /// Check if value is IEnumarable, then cast newValue to Seq and check if isEmpty
-                | :? System.Collections.IEnumerable -> 
-                    let newSeq = newMap.[fieldName]
-                    if newSeq :?> System.Collections.IEnumerable |> Seq.cast |> Seq.isEmpty
-                    then fieldValue 
-                    else newSeq
-                /// Others don't need to be checked as they have no clearly enough defined "empty" state
-                | _ ->
-                    newMap.[fieldName]
-            )
-        FSharpValue.MakeRecord(typeof<'a>, fields) :?> 'a
+    let rec private updateOnlyByExisting (oldVal: obj) (newVal:obj) =      
+        
+        /// try to cast values to types to check for isEmpty according to type.
+        match oldVal with 
+        /// Check if newValue isNull = isEmpty
+        | _ when newVal = null ->
+            oldVal
+        /// Handle OptionTypes
+        // https://stackoverflow.com/questions/6289761/how-to-downcast-from-obj-to-optionobj
+        /// Check of value is option, then check if new value isNone = isEmpty
+        | SomeObj(oldInternal) ->
+            let newOpt = newVal
+            match newOpt with
+            | SomeObj newInternal ->
+                updateOnlyByExisting oldInternal newInternal
+                |> fun v -> makeOptionValue (oldInternal.GetType()) v true
+            | _ -> 
+                oldVal
+        /// Check if value is string, then check if new value is "" = isEmpty
+        | :? String ->
+            let newStr = newVal
+            if string newStr = "" then oldVal else newStr
+        /// https://stackoverflow.com/questions/47280544/determine-if-any-kind-of-list-sequence-array-or-ienumerable-is-empty
+        /// Check if value is IEnumarable, then cast newValue to Seq and check if isEmpty
+        | :? System.Collections.IEnumerable -> 
+            let newSeq = newVal
+            if newSeq :?> System.Collections.IEnumerable |> Seq.cast |> Seq.isEmpty
+            then oldVal 
+            else newSeq
+        /// Others don't need to be checked as they have no clearly enough defined "empty" state
+        | _ ->
+            newVal
+        
+        
 
     /// This type specifies the exact manner on how complex types will be updated
     type UpdateOptions = 
@@ -152,6 +165,11 @@ module Update =
             | UpdateAll ->
                 recordType_2
             | UpdateAllAppendLists ->
-                updateAppend recordType_1 recordType_2
+                (FSharp.Reflection.FSharpValue.GetRecordFields recordType_1,FSharp.Reflection.FSharpValue.GetRecordFields recordType_2)
+                ||> Array.map2 updateAppend
+                |> fun fields -> FSharpValue.MakeRecord(typeof<'a>, fields) :?> 'a
             | UpdateByExisting ->
-                updateOnlyByExisting recordType_1 recordType_2
+                (FSharp.Reflection.FSharpValue.GetRecordFields recordType_1,FSharp.Reflection.FSharpValue.GetRecordFields recordType_2)
+                ||> Array.map2 updateOnlyByExisting
+                |> fun fields -> FSharpValue.MakeRecord(typeof<'a>, fields) :?> 'a
+                
