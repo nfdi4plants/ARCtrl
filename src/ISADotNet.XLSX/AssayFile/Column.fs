8de1b95d010896c7c9ce190eec1e49b603c0c643
Heinrich Weil <hl.weil@t-online.de>
2021-02-21 18:12:10 +0100

    add additional assay file column parsers

--- src/ISADotNet.XLSX/AssayFile/Column.fs
+++ src/ISADotNet.XLSX/AssayFile/Column.fs
@@ -31,7 +31,7 @@ module Value =
 
 module Column =
 
-    type HeaderInfo =
+    type SwateHeader =
         {
             HeaderString : string
             Kind : string
@@ -48,8 +48,9 @@ module Column =
             }
 
         static member fromStringHeader header =
-        
+                  
             let nameRegex = Regex.Match(header,@"(?<= \[).*(?=\])")
+            let kindRegex = Regex.Match(header,@".*(?= \(#.*\))")
             if nameRegex.Success then
                 let kind = Regex.Match(header,@".*(?= \[)")
                 let ontologySourceRegex = Regex.Match(header,@"(?<=#t)\S+:\d*")
@@ -60,9 +61,14 @@ module Column =
                         ontologySourceRegex.Value.Split ':'
                         |> fun o -> o.[0], o.[1]
                     else "", ""
-                HeaderInfo.create header kind.Value (Some (OntologyAnnotation.fromString nameRegex.Value termSource termAccession)) number
+                SwateHeader.create header kind.Value (Some (OntologyAnnotation.fromString nameRegex.Value termAccession termSource)) number
+            elif kindRegex.Success then
+                let kind = kindRegex.Value
+                let numberRegex = Regex.Match(header,@"(?<= \(#)\d+")
+                let number = if numberRegex.Success then Some (int numberRegex.Value) else None
+                SwateHeader.create header kind None number
             else
-                HeaderInfo.create header header None None
+                SwateHeader.create header header None None
         
 
     let mergeOntology (termSourceHeaderOntology : OntologyAnnotation Option) (termAccessionHeaderOntology : OntologyAnnotation Option) =
@@ -73,8 +79,8 @@ module Column =
         | None, None -> None
 
     
-    let tryParseTermSourceReferenceHeader (termHeader:HeaderInfo) (header:string) =
-        match HeaderInfo.fromStringHeader header with
+    let tryParseTermSourceReferenceHeader (termHeader:SwateHeader) (header:string) =
+        match SwateHeader.fromStringHeader header with
         | h when h.Kind = "Term Source REF" && h.Number = termHeader.Number -> 
             match h.Term,termHeader.Term with
             | None, None -> Some h
@@ -82,8 +88,8 @@ module Column =
             | _ -> None
         | _ -> None
     
-    let tryParseTermAccessionNumberHeader (termHeader:HeaderInfo) (header:string) =
-        match HeaderInfo.fromStringHeader header with
+    let tryParseTermAccessionNumberHeader (termHeader:SwateHeader) (header:string) =
+        match SwateHeader.fromStringHeader header with
         | h when h.Kind = "Term Accession Number"  && h.Number = termHeader.Number -> 
             match h.Term,termHeader.Term with
             | None, None -> Some h
@@ -92,18 +98,41 @@ module Column =
         | _ -> None
     
     let tryParseParameterHeader (header:string) =
-        match HeaderInfo.fromStringHeader header with
-        | h when h.Kind = "Parameter" ->
+        match SwateHeader.fromStringHeader header with
+        | h when h.Kind = "Parameter" || h.Kind = "Parameter Value" ->
             Some h
         | _ -> None
     
+    let tryParseFactorHeader (header:string) =
+        match SwateHeader.fromStringHeader header with
+        | h when h.Kind = "Factor" || h.Kind = "Factor Value" ->
+            Some h
+        | _ -> None
+
+    let tryParseCharacteristicsHeader (header:string) =
+        match SwateHeader.fromStringHeader header with
+        | h when h.Kind = "Characteristics" || h.Kind = "Characteristics Value" ->
+            Some h
+        | _ -> None
+
     let tryParseUnitHeader (header:string) =
-        match HeaderInfo.fromStringHeader header with
+        match SwateHeader.fromStringHeader header with
         | h when h.Kind = "Unit" ->
             Some h
+        | _ -> None   
+    
+    let tryParseSampleName (header:string) =
+        match SwateHeader.fromStringHeader header with
+        | h when h.Kind = "Sample Name" ->
+            Some h
         | _ -> None
-    
-    
+
+    let tryParseSourceName (header:string) =
+        match SwateHeader.fromStringHeader header with
+        | h when h.Kind = "Source Name" ->
+            Some h
+        | _ -> None
+
     let tryGetUnitGetterFunction (headers:string seq) =
         Seq.tryPick tryParseUnitHeader headers
         |> Option.map (fun h -> 
@@ -175,7 +204,8 @@ module Column =
                         (termSourceGetter matrix i)
                     
             let category = mergeOntology category1 category2 |> Option.map (Some >> ProtocolParameter.create None)
-    
+            
+            category,
             fun (matrix : System.Collections.Generic.Dictionary<(string * int),string>) i ->
                 ProcessParameterValue.create 
                     category
@@ -183,3 +213,116 @@ module Column =
                     (unitGetter |> Option.map (fun f -> f matrix i))
         )
     
+    let tryGetFactorGetterFunction (headers:string seq) =
+        Seq.tryPick tryParseFactorHeader headers
+        |> Option.map (fun h -> 
+            let unitGetter = tryGetUnitGetterFunction headers
+                  
+            let category1, termAccessionGetter =
+                match Seq.tryPick (tryParseTermAccessionNumberHeader h) headers with
+                | Some h ->
+                    h.Term,
+                    fun matrix i -> 
+                        match Dictionary.tryGetValue (h.HeaderString,i) matrix with
+                        | Some "user-specific" -> None
+                        | Some v -> Some v
+                        | _ -> None 
+                | None -> None, fun _ _ -> None
+            let category2, termSourceGetter =
+                match Seq.tryPick (tryParseTermSourceReferenceHeader h) headers with
+                | Some h ->
+                    h.Term,
+                    fun matrix i -> 
+                        match Dictionary.tryGetValue (h.HeaderString,i) matrix with
+                        | Some "user-specific" -> None
+                        | Some v -> Some v
+                        | _ -> None 
+                | None -> None, fun _ _ -> None
+    
+            let valueGetter = 
+                fun matrix i ->
+                    let v = 
+                        match Dictionary.tryGetValue (h.HeaderString,i) matrix with
+                        | Some "user-specific" -> None
+                        | Some v -> Some v
+                        | _ -> None 
+                    Value.fromOptions 
+                        v
+                        (termAccessionGetter matrix i)
+                        (termSourceGetter matrix i)
+                    
+            let factor = 
+                mergeOntology category1 category2 
+                |> Option.map (fun oa ->  Factor.create None (oa.Name |> Option.map AnnotationValue.toString) (Some oa) None)
+            
+            factor,
+            fun (matrix : System.Collections.Generic.Dictionary<(string * int),string>) i ->
+                FactorValue.create 
+                    None
+                    factor
+                    (valueGetter matrix i)
+                    (unitGetter |> Option.map (fun f -> f matrix i))
+        )
+
+    let tryGetCharacteristicGetterFunction (headers:string seq) =
+        Seq.tryPick tryParseCharacteristicsHeader headers
+        |> Option.map (fun h -> 
+            let unitGetter = tryGetUnitGetterFunction headers
+                  
+            let category1, termAccessionGetter =
+                match Seq.tryPick (tryParseTermAccessionNumberHeader h) headers with
+                | Some h ->
+                    h.Term,
+                    fun matrix i -> 
+                        match Dictionary.tryGetValue (h.HeaderString,i) matrix with
+                        | Some "user-specific" -> None
+                        | Some v -> Some v
+                        | _ -> None 
+                | None -> None, fun _ _ -> None
+            let category2, termSourceGetter =
+                match Seq.tryPick (tryParseTermSourceReferenceHeader h) headers with
+                | Some h ->
+                    h.Term,
+                    fun matrix i -> 
+                        match Dictionary.tryGetValue (h.HeaderString,i) matrix with
+                        | Some "user-specific" -> None
+                        | Some v -> Some v
+                        | _ -> None 
+                | None -> None, fun _ _ -> None
+    
+            let valueGetter = 
+                fun matrix i ->
+                    let v = 
+                        match Dictionary.tryGetValue (h.HeaderString,i) matrix with
+                        | Some "user-specific" -> None
+                        | Some v -> Some v
+                        | _ -> None 
+                    Value.fromOptions 
+                        v
+                        (termAccessionGetter matrix i)
+                        (termSourceGetter matrix i)
+                    
+            let characteristic = mergeOntology category1 category2 |> Option.map (Some >> MaterialAttribute.create None)            
+            
+            characteristic,
+            fun (matrix : System.Collections.Generic.Dictionary<(string * int),string>) i ->
+                MaterialAttributeValue.create 
+                    None
+                    characteristic
+                    (valueGetter matrix i)
+                    (unitGetter |> Option.map (fun f -> f matrix i))
+        )
+
+    let tryGetSampleNameGetter (headers:string seq) =
+        Seq.tryPick tryParseSampleName headers
+        |> Option.map (fun h -> 
+            fun (matrix : System.Collections.Generic.Dictionary<(string * int),string>) i ->
+                Dictionary.tryGetValue (h.HeaderString,i) matrix
+        )
+
+    let tryGetSourceNameGetter (headers:string seq) =
+        Seq.tryPick tryParseSampleName headers
+        |> Option.map (fun h -> 
+            fun (matrix : System.Collections.Generic.Dictionary<(string * int),string>) i ->
+                Dictionary.tryGetValue (h.HeaderString,i) matrix
+        )
