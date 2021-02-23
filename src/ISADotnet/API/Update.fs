dc98abe8b4fd386ed5f1f0743482dc8ffa443865
Heinrich Weil <hl.weil@t-online.de>
2021-02-23 16:29:32 +0100

    add UpdateByExistingAppendLists option to record type updater

--- src/ISADotnet/API/Update.fs
+++ src/ISADotnet/API/Update.fs
@@ -144,7 +144,63 @@ module Update =
         | _ ->
             newVal
         
+    /// updates oldRT with newRT by replacing all values, but only if the new value is not empty.
+    let rec private updateOnlyByExistingAppend (oldVal: obj) (newVal:obj) =      
         
+        /// try to cast values to types to check for isEmpty according to type.
+        match oldVal with 
+        /// Check if newValue isNull = isEmpty
+        | _ when newVal = null ->
+            oldVal
+        /// Handle OptionTypes
+        // https://stackoverflow.com/questions/6289761/how-to-downcast-from-obj-to-optionobj
+        /// Check of value is option, then check if new value isNone = isEmpty
+        | SomeObj(oldInternal) ->
+            let newOpt = newVal
+            match newOpt with
+            | SomeObj newInternal ->
+                updateOnlyByExistingAppend oldInternal newInternal
+                |> fun v -> makeOptionValue (oldInternal.GetType()) v true
+            | _ -> 
+                oldVal
+        /// Check if value is string, then check if new value is "" = isEmpty
+        | :? String ->
+            let newStr = newVal
+            if string newStr = "" then oldVal else newStr
+        /// https://stackoverflow.com/questions/47280544/determine-if-any-kind-of-list-sequence-array-or-ienumerable-is-empty
+        /// Check if value is IEnumarable, then cast newValue to Seq and check if isEmpty
+        | :? System.Collections.IEnumerable -> 
+            let oldSeq = oldVal
+            let newSeq = newVal
+            let fieldT = oldVal.GetType()
+            /// Maps are IEnumerables but are not easily to append. TODO(?)
+            let isMap =
+                let genericMap = typeof<Map<_,_>>
+                fieldT.Name = genericMap.Name
+            /// t is the type of the IEnumerable elements.
+            let t =
+                /// element types are accessed differently for (list, seq) and Array. 
+                if fieldT.IsArray then fieldT.GetElementType() else
+                    oldVal.GetType().GetGenericArguments() |> Array.head
+            /// This function accesses the append method of the list/array module and applies it accordingly to the element type.
+            let appendGenericListsByType l1 l2 (t:Type) =
+                // https://stackoverflow.com/questions/41253131/how-to-create-an-empty-list-of-a-specific-runtime-type
+                System.Reflection.Assembly
+                    .GetAssembly(typeof<_ list>)
+                    .GetType(if fieldT.IsArray then "Microsoft.FSharp.Collections.ArrayModule" else "Microsoft.FSharp.Collections.ListModule")
+                    .GetMethod("Append")
+                    .MakeGenericMethod(t)
+                    .Invoke(null, [|l1;l2|])
+            /// If the IEnumerable is a map then we just replace with the new entry.
+            if isMap then 
+                newVal
+            else
+                let r = 
+                    appendGenericListsByType oldSeq newSeq t
+                r |> box
+        /// Others don't need to be checked as they have no clearly enough defined "empty" state
+        | _ ->
+            newVal  
 
     /// This type specifies the exact manner on how complex types will be updated
     type UpdateOptions = 
@@ -154,10 +210,15 @@ module Update =
         ///
         /// Empty string = ""; Empty IEnumerable (Set, List, Array, Seq, Map); Empty Option = None; null
         | UpdateByExisting
-        /// Updates all existing fields by replacing them with the corresponding new fields, except any lists which will be appended with the new list shown first
+        /// Updates all existing fields by replacing them with the corresponding new fields, except any lists which will be appended
         ///
         /// Maps are currently not appended
         | UpdateAllAppendLists
+        /// Updates all existing fields by replacing them with the corresponding new fields if the new field is not empty, except any lists which will be appended
+        ///
+        /// Empty string = ""; Empty IEnumerable (Set, List, Array, Seq, Map); Empty Option = None; null
+        | UpdateByExistingAppendLists
+
 
         /// This function will update recordType_1 with the values given in recordType_2 as specified by UpdateOption.
         member this.updateRecordType (recordType_1:'a) (recordType_2:'a) =    
@@ -172,4 +233,7 @@ module Update =
                 (FSharp.Reflection.FSharpValue.GetRecordFields recordType_1,FSharp.Reflection.FSharpValue.GetRecordFields recordType_2)
                 ||> Array.map2 updateOnlyByExisting
                 |> fun fields -> FSharpValue.MakeRecord(typeof<'a>, fields) :?> 'a
-                
+            | UpdateByExistingAppendLists ->
+                (FSharp.Reflection.FSharpValue.GetRecordFields recordType_1,FSharp.Reflection.FSharpValue.GetRecordFields recordType_2)
+                ||> Array.map2 updateOnlyByExistingAppend
+                |> fun fields -> FSharpValue.MakeRecord(typeof<'a>, fields) :?> 'a
