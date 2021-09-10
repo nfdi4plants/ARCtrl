0847b38329b73b3e3c7b14f884b91d6a43849c52
Heinrich Weil <hl.weil@t-online.de>
2021-09-10 15:39:49 +0200

    init ISADotNet.Viz

---
+++ src/ISADotNet.Viz/DAG.fs
@@ -0,0 +1,13 @@
+﻿namespace ISADotNet.Viz
+
+open ISADotNet
+open Cyjs.NET
+
+module DAG =
+
+    let show (dag : CyGraph.CyGraph) = CyGraph.show dag
+
+    let toEmbeddedHTML (dag : CyGraph.CyGraph) = HTML.toEmbeddedHTML dag
+
+    let toHTML (dag : CyGraph.CyGraph) = HTML.toCytoHTML dag
+
