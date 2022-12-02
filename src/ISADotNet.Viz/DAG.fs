6d5ee0c665f65d79c3b6653f702551684cda8f82
Heinrich Lukas Weil <hl.weil@t-online.de>
2022-12-02 18:17:59 +0100

    make DAG creation more robust against missing names fix #51

--- src/ISADotNet.Viz/DAG.fs
+++ src/ISADotNet.Viz/DAG.fs
@@ -19,14 +19,26 @@ type DAG =
 
         let rootNodes = API.ProcessSequence.getRootInputs ps |> List.map ProcessInput.getName
 
+        let makeSampleName (processName : string option) (processId : int) (nameroot : string) (materialId : int) =
+            match processName with
+            | Some n -> $"{n}_{nameroot}{materialId}"
+            | None -> $"Process{processId}_{nameroot}{materialId}"
+
         let edges = 
             ps
-            |> List.collect (fun p ->
+            |> List.mapi (fun pi p ->
+                if p.Inputs = None then failwith $"Could no create DAG from Process sequence, process {p.Name} has no inputs"
+                if p.Outputs = None then failwith $"Could no create DAG from Process sequence, process {p.Name} has no outputs"
                 p.Outputs.Value
                 |> List.zip p.Inputs.Value
-                |> List.map (fun (i,o) -> ProcessInput.getName i ,ProcessOutput.getName o ,p.Name.Value)
+                |> List.mapi (fun (mi) (i,o) -> 
+                    (ProcessInput.tryGetName i |> function | Some n -> n | None -> makeSampleName p.Name pi "Input" mi),
+                    (ProcessOutput.tryGetName o |> function | Some n -> n | None -> makeSampleName p.Name pi "Output" mi),
+                    p.Name |> function | Some n -> n | None -> $"Process{pi}"
+                )
                 |> List.distinct
             )
+            |> List.concat
             |> List.append (rootNodes |> List.map (fun i -> "Root",i,""))
                           
         let cyNodes = 
