491ff31d6cf2bcbd7bbf4e8b6b316a5d34c9d2c4
Kevin F <Freymaurer@gmx.de>
2023-05-08 21:51:51 +0200

    Update releasenotes task #88

--- build/ReleaseNotesTasks.fs
+++ build/ReleaseNotesTasks.fs
@@ -2,6 +2,7 @@
 
 open Fake.Extensions.Release
 open BlackFox.Fake
+open Fake.Core
 
 /// This might not be necessary, mostly useful for apps which want to display current version as it creates an accessible F# version script from RELEASE_NOTES.md
 let createAssemblyVersion = BuildTask.create "createvfs" [] {
@@ -11,6 +12,17 @@ let createAssemblyVersion = BuildTask.create "createvfs" [] {
 // https://github.com/Freymaurer/Fake.Extensions.Release#releaseupdate
 let updateReleaseNotes = BuildTask.createFn "ReleaseNotes" [] (fun config ->
     ReleaseNotes.update(ProjectInfo.gitOwner, ProjectInfo.project, config)
+
+    let semVer = 
+        Fake.Core.ReleaseNotes.load "RELEASE_NOTES.md"
+        |> fun x -> x.SemVer.AsString
+    Trace.trace "Start updating package.json version"
+    // Update Version in src/Nfdi4Plants.Fornax.Template/package.json
+    let p = "package.json"
+    let t = System.IO.File.ReadAllText p
+    let tNew = System.Text.RegularExpressions.Regex.Replace(t, """\"version\": \".*\",""", sprintf "\"version\": \"%s\"," semVer )
+    System.IO.File.WriteAllText(p, tNew)
+    Trace.trace "Finish updating package.json version"
 )
 
 
