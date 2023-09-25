a0ab192f150062d597932510576863dc8d5000d3
Heinrich Lukas Weil <hl.weil@t-online.de>
2023-09-25 11:19:51 +0200

    fix url in web request test

--- tests/ARCtrl/WebRequest.Tests.fs
+++ tests/ARCtrl/WebRequest.Tests.fs
@@ -5,7 +5,7 @@ open ARCtrl.WebRequest
 
 let tests_download = testList "Download" [
     testCaseAsync "simple json" <| async {
-        let url = @"https://raw.githubusercontent.com/nfdi4plants/ARCtrl/developer_TemplateRequest/tests/TestingUtils/TestObjects.Json/MinimalJson.json"
+        let url = @"https://raw.githubusercontent.com/nfdi4plants/ARCtrl/developer/tests/TestingUtils/TestObjects.Json/MinimalJson.json"
         let expected = """{"TestKey": "TestValue"}"""
         let! jsonString = downloadFile url 
         Expect.equal jsonString expected "equal"
