146b60caffb8c39bf58684f738868303f76ffef4
Kevin F <Freymaurer@gmx.de>
2023-12-21 11:14:29 +0100

    Make template json use arc json

--- src/ARCtrl/Templates/Template.Json.fs
+++ src/ARCtrl/Templates/Template.Json.fs
@@ -23,44 +23,6 @@ module Organisation =
             |> Decode.succeed
         )
 
-//module CompositeCell =
-
-//    let encode = Encode.Auto.generateEncoder<CompositeCell>()
-
-//    let decode = Decode.Auto.generateDecoder<CompositeCell>()
-
-//module CompositeHeader =
-
-//    let encode = Encode.Auto.generateEncoder<CompositeHeader>()
-
-//    let decode = Decode.Auto.generateDecoder<CompositeHeader>()
-
-//module ArcTable =
-
-//    let encode (table: ArcTable) =
-//        let keyEncoder : Encoder<int*int> = Encode.tuple2 Encode.int Encode.int
-//        let valueEncoder = CompositeCell.encode
-//        Encode.object [
-//            "name", Encode.string table.Name
-//            "header", Encode.list [
-//                for h in table.Headers do yield CompositeHeader.encode h
-//            ]
-//            "values", Encode.map keyEncoder valueEncoder ([for KeyValue(k,v) in table.Values do yield k, v] |> Map)
-//        ] 
-
-//    let decode : Decoder<ArcTable> =
-//        Decode.object(fun get ->
-//            let decodedHeader = get.Required.Field "header" (Decode.list CompositeHeader.decode) |> ResizeArray
-//            let keyDecoder : Decoder<int*int> = Decode.tuple2 Decode.int Decode.int
-//            let valueDecoder = CompositeCell.decode
-//            let decodedValues = get.Required.Field "values" (Decode.map' keyDecoder valueDecoder) |> System.Collections.Generic.Dictionary
-//            ArcTable.create(
-//                get.Required.Field "name" Decode.string,
-//                decodedHeader,
-//                decodedValues
-//            )
-//        )
-
 module Template =
 
     open ARCtrl.ISA.Json
@@ -70,7 +32,7 @@ module Template =
         let oaEncoder = ARCtrl.ISA.Json.OntologyAnnotation.encoder (ConverterOptions())
         Encode.object [
             "id", Encode.guid template.Id
-            "table", template.Table.GetProcesses() |> List.map (Process.encoder (ConverterOptions())) |> Encode.list 
+            "table", ArcTable.encoder template.Table
             "name", Encode.string template.Name
             "description", Encode.string template.Description
             "organisation", Organisation.encode template.Organisation
@@ -93,7 +55,7 @@ module Template =
         Decode.object(fun get ->
             Template.create(
                 get.Required.Field "id" Decode.guid,
-                get.Required.Field "table" (Decode.list (Process.decoder (ConverterOptions()) ) |> Decode.map (fun ps -> ArcTables.fromProcesses ps |> fun arctbls -> arctbls.Tables.[0])),
+                get.Required.Field "table" ArcTable.decoder,
                 get.Required.Field "name" Decode.string,
                 get.Required.Field "description" Decode.string,
                 get.Required.Field "organisation" Organisation.decode,
