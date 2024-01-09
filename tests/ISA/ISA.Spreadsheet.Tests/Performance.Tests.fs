2aca309623061171dc1c3ec3897d2c5cd388bf11
Heinrich Lukas Weil <hl.weil@t-online.de>
2024-01-09 09:39:17 +0100

    Make ArcStudy IO performance test more strict

--- tests/ISA/ISA.Spreadsheet.Tests/Performance.Tests.fs
+++ tests/ISA/ISA.Spreadsheet.Tests/Performance.Tests.fs
@@ -13,7 +13,7 @@ let private tests_Study = testList "Study" [
             let s,_ = ArcStudy.fromFsWorkbook fswb
             let timer_end = System.DateTime.Now
             let runtime = (timer_end - timer_start).Milliseconds
-            let expected = 3000 // this is too high and should be reduced
+            let expected = 300 // this is too high and should be reduced
             Expect.equal s.TableCount 1 "Table count"
             Expect.isTrue (runtime <= expected) $"Expected conversion to be finished in under {expected}, but it took {runtime}"
         convertToArcFile fswb
