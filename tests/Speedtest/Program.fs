3f8c7883f6fc40f1be910afe20b19f0b3d0fad1d
Heinrich Lukas Weil <hl.weil@t-online.de>
2024-01-26 11:20:08 +0100

    add case for speedtest project

--- tests/Speedtest/Program.fs
+++ tests/Speedtest/Program.fs
@@ -4,8 +4,15 @@
 [<EntryPoint>]
 let main argv =
     
-    LargeStudy.createStudy 10000
-    |> LargeStudy.toWorkbook
-    |> LargeStudy.fromWorkbook
-    |> ignore
-    1
+    if Array.contains "--manyStudies" argv then
+        ManyStudies.write() |> ignore
+        1
+    elif Array.contains "--largeStudy" argv then
+
+        LargeStudy.createStudy 10000
+        |> LargeStudy.toWorkbook
+        |> LargeStudy.fromWorkbook
+        |> ignore
+        1
+    else 
+        0
