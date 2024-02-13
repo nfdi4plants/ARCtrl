8635d76d663895326a387ef371faa571ff661756
HLWeil <hl.weil@t-online.de>
2024-02-13 17:34:40 +0100

    Improve speed of GetHashCode

--- src/ISA/ISA/ArcTypes/ArcTable.fs
+++ src/ISA/ISA/ArcTypes/ArcTable.fs
@@ -780,18 +780,11 @@ type ArcTable(name: string, headers: ResizeArray<CompositeHeader>, values: Syste
     // it's good practice to ensure that this behaves using the same fields as Equals does:
     override this.GetHashCode() = 
         //let v1,v2 = 
-        let v =
-            [|
-                for KeyValue(k,v) in this.Values do
-                    yield k, v
-            |] 
-            |> Array.sortBy fst
-            // must remove tuples. Tuples handle unpredictable for GetHashCode in javascript.
-            |> Array.map (fun ((k1,k2),v) -> [|box k1; box k2; box v|] |> Aux.HashCodes.boxHashArray) 
+        let vHash =  ArcTableAux.boxHashValues this.ColumnCount this.Values
         [|
             box this.Name
-            Array.ofSeq >> Aux.HashCodes.boxHashArray <| this.Headers
-            Array.ofSeq >> Aux.HashCodes.boxHashArray <| v
+            this.Headers |> Aux.HashCodes.boxHashSeq 
+            vHash
         |]
         |> Aux.HashCodes.boxHashArray 
         |> fun x -> x :?> int
