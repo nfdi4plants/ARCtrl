7d6c107728970f12bc0e843ec936379ec091bce2
Kevin F <Freymaurer@gmx.de>
2024-04-03 14:46:59 +0200

    Add setup script for windows :construction_worker:

---
+++ setup.cmd
@@ -0,0 +1,15 @@
+@echo off
+TITLE ARCtrl Setup
+
+ECHO Install JavaScript Dependencies
+REM npm is a batch file itself. Must use with "CALL" otherwise will exit afterwards.
+CALL npm i
+
+ECHO Setup Python Virtual Environment
+CALL py -m venv .venv
+
+ECHO Install Python Dependencies
+CALL .\.venv\Scripts\python.exe -m pip install -U pip setuptools
+CALL .\.venv\Scripts\python.exe -m pip install poetry
+CALL .\.venv\Scripts\python.exe -m poetry install --no-root
+ECHO DONE!
