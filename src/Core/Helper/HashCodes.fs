025734d63e0319d74dbd4edbe6433e61e9dd3fca
Kevin F <Freymaurer@gmx.de>
2024-04-04 10:37:53 +0200

    rmv code circumventing fable python bug :fire:

--- src/Core/Helper/HashCodes.fs
+++ src/Core/Helper/HashCodes.fs
@@ -1,16 +1,4 @@
 ﻿module ARCtrl.Helper.HashCodes
-    
-open Fable.Core
-
-// This is necessary until https://github.com/fable-compiler/Fable/issues/3778 is resolved
-
-#if FABLE_COMPILER_PYTHON
-[<Emit("hasattr($0,\"__hash__\")")>]
-let pyHasCustomHash (obj) : bool = nativeOnly
-        
-[<Emit("$0.__hash__()")>]
-let pyCustomHash (obj) : int = nativeOnly
-#endif
 
 let mergeHashes (hash1 : int) (hash2 : int) : int =
     0x9e3779b9 + hash2 + (hash1 <<< 6) + (hash1 >>> 2)
@@ -27,14 +15,6 @@ let hashDateTime (dt : System.DateTime) : int =
     
 
 let hash obj =
-    //#if FABLE_COMPILER_PYTHON
-    //    if pyHasCustomHash obj then
-    //        pyCustomHash obj
-    //    else
-    //        obj.GetHashCode()
-    //#else
-    //    obj.GetHashCode()
-    //#endif
     obj.GetHashCode()
 
 let boxHashOption (a: 'a option) : obj =
