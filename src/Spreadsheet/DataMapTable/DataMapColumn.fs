a55279ac54c1752ed6bb893817dc3e1f31d6af96
HLWeil <hl.weil@t-online.de>
2024-05-10 14:48:31 +0200

    add datamap spreadsheet parsing

---
+++ src/Spreadsheet/DataMapTable/DataMapColumn.fs
@@ -0,0 +1,48 @@
+﻿module ARCtrl.Spreadsheet.DataMapColumn
+
+open ARCtrl
+open ArcTable
+open FsSpreadsheet
+
+let fromFsColumns (columns : list<FsColumn>) : CompositeColumn =
+    let header, cellParser = 
+        columns
+        |> List.map (fun c -> c.[1])
+        |> DataMapHeader.fromFsCells
+    let l = columns.[0].RangeAddress.LastAddress.RowNumber
+    let cells = 
+        [|
+        for i = 2 to l do
+            columns
+            |> List.map (fun c -> c.[i])
+            |> cellParser
+        |]                 
+    CompositeColumn.create(header,cells)
+
+
+let toFsColumns (column : CompositeColumn) : FsCell list list =
+    let isTerm = column.Header.IsTermColumn
+    let isData = column.Header.IsDataColumn
+    let header = DataMapHeader.toFsCells column.Header
+    let cells = column.Cells |> Array.map (CompositeCell.toFsCells isTerm false)
+    if isTerm then
+        [
+            [header.[0]; for i = 0 to column.Cells.Length - 1 do cells.[i].[0]]
+            [header.[1]; for i = 0 to column.Cells.Length - 1 do cells.[i].[1]]
+            [header.[2]; for i = 0 to column.Cells.Length - 1 do cells.[i].[2]]
+        ]
+    elif isData then
+        let hasFormat = column.Cells |> Seq.exists (fun c -> c.AsData.Format.IsSome)
+        let hasSelectorFormat = column.Cells |> Seq.exists (fun c -> c.AsData.SelectorFormat.IsSome)
+
+        [
+            [header.[0]; for i = 0 to column.Cells.Length - 1 do cells.[i].[0]]
+            if hasFormat then 
+                [header.[1]; for i = 0 to column.Cells.Length - 1 do cells.[i].[1]]
+            if hasSelectorFormat then 
+                [header.[2]; for i = 0 to column.Cells.Length - 1 do cells.[i].[2]]
+        ]
+    else
+        [
+            [header.[0]; for i = 0 to column.Cells.Length - 1 do cells.[i].[0]]
+        ]
---
+++ src/Spreadsheet/DataMapTable/DataMapHeader.fs
@@ -0,0 +1,122 @@
+﻿module ARCtrl.Spreadsheet.DataMapHeader
+
+open ARCtrl
+open ARCtrl.Helper
+open FsSpreadsheet
+
+module ActivePattern = 
+
+    open Regex.ActivePatterns
+
+    let (|Term|_|) (categoryString : string) (categoryHeader : CompositeHeader) (cells : FsCell list) : (CompositeHeader*(FsCell list -> CompositeCell)) option =
+        let (|AC|_|) s =
+            if s = categoryString then Some categoryHeader else None
+        let (|TSRColumnHeaderRaw|_|) (s : string) =
+            if s.StartsWith("Term Source REF") then Some s else None
+        let (|TANColumnHeaderRaw|_|) (s : string) =
+            if s.StartsWith("Term Accession Number") then Some s else None
+        let cellValues = cells |> List.map (fun c -> c.ValueAsString())
+        match cellValues with
+        | [AC header] -> 
+            (header, CompositeCell.termFromFsCells None None)
+            |> Some
+        | [AC header; TSRColumnHeaderRaw _; TANColumnHeaderRaw _] ->
+            (header, CompositeCell.termFromFsCells (Some 1) (Some 2))
+            |> Some
+        | [AC header; TANColumnHeaderRaw _; TSRColumnHeaderRaw _] ->
+            (header, CompositeCell.termFromFsCells (Some 2) (Some 1))
+            |> Some
+        | _ -> None
+
+    let (|Explication|_|) (cells : FsCell list) =
+        match cells with
+        | Term DataMapAux.explicationShortHand DataMapAux.explicationHeader r ->
+            Some r
+        | _ -> None
+
+    let (|Unit|_|) (cells : FsCell list) =
+        match cells with
+        | Term DataMapAux.unitShortHand DataMapAux.unitHeader r ->
+            Some r
+        | _ -> None
+
+    let (|ObjectType|_|) (cells : FsCell list) =
+        match cells with
+        | Term DataMapAux.objectTypeShortHand DataMapAux.objectTypeHeader r ->
+            Some r
+        | _ -> None
+
+    let (|Description|_|) (cells : FsCell list) =
+        let cellValues = cells |> List.map (fun c -> c.ValueAsString())
+        match cellValues with
+        | [DataMapAux.descriptionShortHand] -> Some(DataMapAux.descriptionHeader, CompositeCell.freeTextFromFsCells)
+        | _ -> None
+
+    let (|GeneratedBy|_|) (cells : FsCell list) =
+        let cellValues = cells |> List.map (fun c -> c.ValueAsString())
+        match cellValues with
+        | [DataMapAux.generatedByShortHand] -> Some(DataMapAux.generatedByHeader, CompositeCell.freeTextFromFsCells)
+        | _ -> None
+
+    let (|Data|_|) (cells : FsCell list) =
+        let cellValues = cells |> List.map (fun c -> c.ValueAsString())
+        match cellValues with
+        | DataMapAux.dataShortHand :: cols -> 
+            
+            let format = cols |> List.tryFindIndex (fun s -> s.StartsWith("Data Format"))  |> Option.map ((+) 1)
+            let selectorFormat = cols |> List.tryFindIndex (fun s -> s.StartsWith("Data Selector Format"))  |> Option.map ((+) 1)
+            (CompositeHeader.Input (IOType.Data), CompositeCell.dataFromFsCells format selectorFormat)
+            |> Some
+
+        | _ -> None
+
+    let (|FreeText|_|) (cells : FsCell list) =
+        let cellValues = cells |> List.map (fun c -> c.ValueAsString())
+        match cellValues with
+        | [text] -> 
+            (CompositeHeader.FreeText text, CompositeCell.freeTextFromFsCells)
+            |> Some
+        | _ -> None
+
+open ActivePattern
+
+let fromFsCells (cells : list<FsCell>) : CompositeHeader*(FsCell list -> CompositeCell) =
+    match cells with
+    | Data d -> d
+    | Explication e -> e
+    | Unit u -> u
+    | ObjectType ot -> ot
+    | Description d -> d
+    | GeneratedBy gb -> gb
+    | FreeText ft -> ft
+    | _ -> failwithf "Could not parse header group %O" cells
+
+let toFsCells (header : CompositeHeader) : list<FsCell> = 
+    match header with
+    | CompositeHeader.Input IOType.Data ->      
+        [
+            FsCell("Data")
+            FsCell("Data Format")
+            FsCell("Data Selector Format")
+        ]                 
+    | h when h = DataMapAux.explicationHeader -> 
+        [
+            FsCell(DataMapAux.explicationShortHand)
+            FsCell("Term Source REF")
+            FsCell("Term Accession Number")
+        ]
+    | h when h = DataMapAux.unitHeader -> 
+        [
+            FsCell(DataMapAux.unitShortHand)
+            FsCell("Term Source REF")
+            FsCell("Term Accession Number")
+        ]
+    | h when h = DataMapAux.objectTypeHeader -> 
+        [
+            FsCell(DataMapAux.objectTypeShortHand)
+            FsCell("Term Source REF")
+            FsCell("Term Accession Number")
+        ]
+    | CompositeHeader.FreeText text -> 
+        [FsCell(text)]
+    | _ -> failwithf "Could not parse DataMap header %O." header
