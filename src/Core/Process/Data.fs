08fcb3b32c287e0f1fb9d4876905a15382758e7d
HLWeil <hl.weil@t-online.de>
2024-05-27 16:48:42 +0200

    update Data and CompositeCell json parsing

--- src/Core/Process/Data.fs
+++ src/Core/Process/Data.fs
@@ -3,26 +3,45 @@ namespace ARCtrl
 open ARCtrl
 open ARCtrl.Process
 open ARCtrl.Helper 
+open Fable.Core
 
-type Data = 
-    {
-        ID : URI option
-        Name : string option
-        DataType : DataFile option
-        Format : string option
-        SelectorFormat : URI option
-        Comments : Comment list option
-    }
+[<AttachMembers>]
+type Data(?id,?name,?dataType,?format,?selectorFormat,?comments) =
+
+    let mutable _id : URI option = id
+    let mutable _name : string option = name
+    let mutable _dataType : DataFile option = dataType
+    let mutable _format : string option = format
+    let mutable _selectorFormat : URI option = selectorFormat
+    let mutable _comments : Comment ResizeArray = Option.defaultValue (ResizeArray()) comments
+    
+    member this.ID
+        with get() = _id
+        and set(id) = _id <- id
+
+    member this.Name
+        with get() = _name
+        and set(name) = _name <- name
+
+    member this.DataType
+        with get() = _dataType
+        and set(dataType) = _dataType <- dataType
+
+    member this.Format
+        with get() = _format
+        and set(format) = _format <- format
+
+    member this.SelectorFormat
+        with get() = _selectorFormat
+        and set(selectorFormat) = _selectorFormat <- selectorFormat
+
+    member this.Comments
+        with get() = _comments
+        and set(comments) = _comments <- comments
+    
 
     static member make id name dataType format selectorFormat comments =
-        {
-            ID      = id
-            Name    = name
-            DataType = dataType
-            Format  = format
-            SelectorFormat = selectorFormat
-            Comments = comments         
-        }
+        Data(?id=id,?name=name,?dataType=dataType,?format=format,?selectorFormat=selectorFormat,?comments=comments)
 
     static member create (?Id,?Name,?DataType,?Format,?SelectorFormat,?Comments) = 
         Data.make Id Name DataType Format SelectorFormat Comments
@@ -33,6 +52,25 @@ type Data =
     member this.NameText =
         this.Name
         |> Option.defaultValue ""
+
+    member this.Copy() =
+        let nextComments = this.Comments |> ResizeArray.map (fun c -> c.Copy())
+        Data(?id=this.ID,?name=this.Name,?dataType=this.DataType,?format=this.Format,?selectorFormat=this.SelectorFormat,comments=nextComments)
+
+    override this.GetHashCode() =
+        [|
+            HashCodes.boxHashOption this.ID
+            HashCodes.boxHashOption this.Name
+            HashCodes.boxHashOption this.DataType
+            HashCodes.boxHashOption this.Format
+            HashCodes.boxHashOption this.SelectorFormat
+            HashCodes.boxHashSeq this.Comments
+        |]
+        |> HashCodes.boxHashArray
+        |> fun x -> x :?> int
+
+    override this.Equals(obj) =
+        HashCodes.hash this = HashCodes.hash obj
 
     interface IISAPrintable with
         member this.Print() = 
--- src/Json/Process/Data.fs
+++ src/Json/Process/Data.fs
@@ -2,10 +2,68 @@ namespace ARCtrl.Json
 
 open Thoth.Json.Core
 
+open StringTable
+
 open ARCtrl
 open ARCtrl.Process
 
 module Data = 
+
+    // let mutable _id : URI option = id
+    // let mutable _name : string option = name
+    // let mutable _dataType : DataFile option = dataType
+    // let mutable _format : string option = format
+    // let mutable _selectorFormat : URI option = selectorFormat
+    // let mutable _comments : Comment list option = comments
+    let encoder (d : Data) = 
+        [
+            Encode.tryInclude "@id" Encode.string d.ID
+            Encode.tryInclude "name" Encode.string d.Name
+            Encode.tryInclude "dataType" DataFile.ISAJson.encoder d.DataType
+            Encode.tryInclude "format" Encode.string d.Format
+            Encode.tryInclude "selectorFormat" Encode.string d.SelectorFormat
+            Encode.tryIncludeSeq "comments" Comment.encoder d.Comments
+        ]
+        |> Encode.choose
+        |> Encode.object
+
+
+    let decoder : Decoder<Data> =
+        Decode.object (fun get ->
+            Data(
+                ?id = get.Optional.Field "@id" Decode.uri,
+                ?name = get.Optional.Field "name" Decode.string,
+                ?dataType = get.Optional.Field "dataType" DataFile.ISAJson.decoder,
+                ?format = get.Optional.Field "format" Decode.string,
+                ?selectorFormat = get.Optional.Field "selectorFormat" Decode.uri,
+                ?comments = get.Optional.Field "comments" (Decode.resizeArray Comment.decoder)
+            )
+        )
+
+    let compressedEncoder (stringTable : StringTableMap) (d : Data) = 
+        [
+            Encode.tryInclude "i" (StringTable.encodeString stringTable) d.ID
+            Encode.tryInclude "n" (StringTable.encodeString stringTable) d.Name
+            Encode.tryInclude "d" DataFile.ISAJson.encoder d.DataType
+            Encode.tryInclude "f" (StringTable.encodeString stringTable) d.Format
+            Encode.tryInclude "s" (StringTable.encodeString stringTable) d.SelectorFormat
+            Encode.tryIncludeSeq "c" Comment.encoder d.Comments
+        ]
+        |> Encode.choose
+        |> Encode.object
+
+
+    let compressedDecoder (stringTable : StringTableArray) : Decoder<Data> =
+        Decode.object (fun get ->
+            Data(
+                ?id = get.Optional.Field "i" (StringTable.decodeString stringTable),
+                ?name = get.Optional.Field "n" (StringTable.decodeString stringTable),
+                ?dataType = get.Optional.Field "d" DataFile.ISAJson.decoder,
+                ?format = get.Optional.Field "f" (StringTable.decodeString stringTable),
+                ?selectorFormat = get.Optional.Field "s" (StringTable.decodeString stringTable),
+                ?comments = get.Optional.Field "c" (Decode.resizeArray Comment.decoder)
+            )
+        )
 
     module ROCrate =
 
@@ -24,7 +82,7 @@ module Data =
                 Encode.tryInclude "type" DataFile.ROCrate.encoder oa.DataType
                 Encode.tryInclude "encodingFormat" Encode.string oa.Format
                 Encode.tryInclude "usageInfo" Encode.string oa.SelectorFormat
-                Encode.tryIncludeListOpt "comments" Comment.ROCrate.encoder oa.Comments
+                Encode.tryIncludeSeq "comments" Comment.ROCrate.encoder oa.Comments
                 "@context", ROCrateContext.Data.context_jsonvalue
             ]
             |> Encode.choose
@@ -32,14 +90,14 @@ module Data =
 
         let decoder : Decoder<Data> =
             Decode.object (fun get ->
-                {
-                    ID = get.Optional.Field "@id" Decode.uri
-                    Name = get.Optional.Field "name" Decode.string
-                    DataType = get.Optional.Field "type" DataFile.ROCrate.decoder
-                    Format = get.Optional.Field "encodingFormat" Decode.string
-                    SelectorFormat = get.Optional.Field "usageInfo" Decode.uri
-                    Comments = get.Optional.Field "comments" (Decode.list Comment.ROCrate.decoder)
-                }
+                Data(
+                    ?id = get.Optional.Field "@id" Decode.uri,
+                    ?name = get.Optional.Field "name" Decode.string,
+                    ?dataType = get.Optional.Field "type" DataFile.ROCrate.decoder,
+                    ?format = get.Optional.Field "encodingFormat" Decode.string,
+                    ?selectorFormat = get.Optional.Field "usageInfo" Decode.uri,
+                    ?comments = get.Optional.Field "comments" (Decode.resizeArray Comment.ROCrate.decoder)
+                )
             
             )
 
@@ -51,7 +109,7 @@ module Data =
                 Encode.tryInclude "@id" Encode.string oa.ID
                 Encode.tryInclude "name" Encode.string oa.Name
                 Encode.tryInclude "type" DataFile.ISAJson.encoder oa.DataType
-                Encode.tryIncludeListOpt "comments" Comment.ISAJson.encoder oa.Comments
+                Encode.tryIncludeSeq "comments" Comment.ISAJson.encoder oa.Comments
             ]
             |> Encode.choose
             |> Encode.object
@@ -60,14 +118,12 @@ module Data =
 
         let decoder: Decoder<Data> =
             Decode.objectNoAdditionalProperties allowedFields (fun get ->
-                {
-                    ID = get.Optional.Field "@id" Decode.uri
-                    Name = get.Optional.Field "name" Decode.string
-                    DataType = get.Optional.Field "type" DataFile.ISAJson.decoder
-                    Format = None
-                    SelectorFormat = None
-                    Comments = get.Optional.Field "comments" (Decode.list Comment.ISAJson.decoder)
-                }
+                Data(
+                    ?id = get.Optional.Field "@id" Decode.uri,
+                    ?name = get.Optional.Field "name" Decode.string,
+                    ?dataType = get.Optional.Field "type" DataFile.ISAJson.decoder,
+                    ?comments = get.Optional.Field "comments" (Decode.resizeArray Comment.ISAJson.decoder)
+                )
             )
 
 [<AutoOpen>]
