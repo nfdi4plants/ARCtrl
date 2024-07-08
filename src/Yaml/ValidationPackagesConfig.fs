cc879fd276e25d4ead4c9b5051f839567e1a745e
Kevin Schneider <schneider.kev@outlook.de>
2024-07-08 10:58:17 +0200

    Add ARCtrl.Yaml: - Uses YAMLicious - tries to mirror ARCtrl.Json - Encoder/Decoder logic for ValidatioNpackages type model

---
+++ src/Yaml/ValidationPackagesConfig.fs
@@ -0,0 +1,47 @@
+﻿namespace ARCtrl.Yaml
+
+open ARCtrl.ValidationPackages
+open YAMLicious
+open YAMLicious.YAMLiciousTypes
+
+module ValidationPackagesConfig = 
+
+    let encoder (validationpackage : ValidationPackagesConfig) = 
+        [
+            "validation_packages", Encode.array ValidationPackage.encoder validationpackage.ValidationPackages
+            Encode.tryInclude "arc_specification" Encode.string  (validationpackage.ARCSpecification)
+        ]
+        |> Encode.choose
+        |> Encode.object
+
+    let decoder : (YAMLElement -> ValidationPackagesConfig) = 
+        Decode.object (fun get ->
+            ValidationPackagesConfig(
+                validation_packages = get.Required.Field "validation_packages" (Decode.array ValidationPackage.decoder),
+                ?arc_specification = get.Optional.Field "arc_specification" Decode.string
+            )
+        )
+
+[<AutoOpen>]
+module ValidationPackageConfigExtensions =
+
+    open Arctrl.Yaml
+    type ValidationPackagesConfig with
+
+        static member fromYamlString (s:string)  = 
+            Decode.fromYamlString ValidationPackagesConfig.decoder s
+
+        static member toYamlString(?whitespace) = 
+            fun (vp:ValidationPackagesConfig) ->
+                ValidationPackagesConfig.encoder vp
+                |> Encode.toYamlString (Encode.defaultWhitespace whitespace)                  
+
+        member this.toYamlString(?whitespace) = 
+            ValidationPackagesConfig.toYamlString(?whitespace=whitespace) this
+
+    //let fromFile (path : string) = 
+    //    File.ReadAllText path 
+    //    |> fromString
+
+    //let toFile (path : string) (c:Comment) = 
+    //    File.WriteAllText(path,toString c)
