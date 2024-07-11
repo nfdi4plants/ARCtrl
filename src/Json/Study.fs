5cfffe92e47ffd342a44debcf50ea0d08ef27359
Kevin F <Freymaurer@gmx.de>
2024-07-11 10:54:54 +0200

    fix study datamap json key name :bug:

--- src/Json/Study.fs
+++ src/Json/Study.fs
@@ -37,7 +37,7 @@ module Study =
             Encode.tryIncludeSeq "Contacts" Person.encoder study.Contacts
             Encode.tryIncludeSeq "StudyDesignDescriptors" OntologyAnnotation.encoder study.StudyDesignDescriptors
             Encode.tryIncludeSeq "Tables" ArcTable.encoder study.Tables
-            Encode.tryInclude "Datamap" DataMap.encoder study.DataMap
+            Encode.tryInclude "DataMap" DataMap.encoder study.DataMap
             Encode.tryIncludeSeq "RegisteredAssayIdentifiers" Encode.string study.RegisteredAssayIdentifiers
             Encode.tryIncludeSeq "Comments" Comment.encoder study.Comments
         ]
@@ -56,7 +56,7 @@ module Study =
                 ?contacts = get.Optional.Field "Contacts" (Decode.resizeArray Person.decoder),
                 ?studyDesignDescriptors = get.Optional.Field "StudyDesignDescriptors" (Decode.resizeArray OntologyAnnotation.decoder),
                 ?tables = get.Optional.Field "Tables" (Decode.resizeArray ArcTable.decoder),
-                ?datamap = get.Optional.Field "Datamap" DataMap.decoder,
+                ?datamap = get.Optional.Field "DataMap" DataMap.decoder,
                 ?registeredAssayIdentifiers = get.Optional.Field "RegisteredAssayIdentifiers" (Decode.resizeArray Decode.string),
                 ?comments = get.Optional.Field "Comments" (Decode.resizeArray Comment.decoder)
             ) 
@@ -77,7 +77,7 @@ module Study =
             Encode.tryIncludeSeq "Contacts" Person.encoder study.Contacts
             Encode.tryIncludeSeq "StudyDesignDescriptors" OntologyAnnotation.encoder study.StudyDesignDescriptors
             Encode.tryIncludeSeq "Tables" (ArcTable.encoderCompressed stringTable oaTable cellTable) study.Tables
-            Encode.tryInclude "Datamap" (DataMap.encoderCompressed stringTable oaTable cellTable) study.DataMap
+            Encode.tryInclude "DataMap" (DataMap.encoderCompressed stringTable oaTable cellTable) study.DataMap
             Encode.tryIncludeSeq "RegisteredAssayIdentifiers" Encode.string study.RegisteredAssayIdentifiers
             Encode.tryIncludeSeq "Comments" Comment.encoder study.Comments
         ]
