36ab0864dfda70ed078cef089dcc9f3e0847341a
Kevin F <Freymaurer@gmx.de>
2024-07-11 09:59:00 +0200

    Added missing datamap to compressed study json :bug:

--- src/Json/Study.fs
+++ src/Json/Study.fs
@@ -77,6 +77,7 @@ module Study =
             Encode.tryIncludeSeq "Contacts" Person.encoder study.Contacts
             Encode.tryIncludeSeq "StudyDesignDescriptors" OntologyAnnotation.encoder study.StudyDesignDescriptors
             Encode.tryIncludeSeq "Tables" (ArcTable.encoderCompressed stringTable oaTable cellTable) study.Tables
+            Encode.tryInclude "Datamap" (DataMap.encoderCompressed stringTable oaTable cellTable) study.DataMap
             Encode.tryIncludeSeq "RegisteredAssayIdentifiers" Encode.string study.RegisteredAssayIdentifiers
             Encode.tryIncludeSeq "Comments" Comment.encoder study.Comments
         ]
@@ -95,6 +96,7 @@ module Study =
                 ?contacts = get.Optional.Field "Contacts" (Decode.resizeArray Person.decoder),
                 ?studyDesignDescriptors = get.Optional.Field "StudyDesignDescriptors" (Decode.resizeArray OntologyAnnotation.decoder),
                 ?tables = get.Optional.Field "Tables" (Decode.resizeArray <| ArcTable.decoderCompressed stringTable oaTable cellTable),
+                ?datamap = get.Optional.Field "DataMap" (DataMap.decoderCompressed stringTable oaTable cellTable),
                 ?registeredAssayIdentifiers = get.Optional.Field "RegisteredAssayIdentifiers" (Decode.resizeArray Decode.string),
                 ?comments = get.Optional.Field "Comments" (Decode.resizeArray Comment.decoder)
             ) 
