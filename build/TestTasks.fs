25c8d3caadcdf9e005a3a45ba908a028081d8561
Heinrich Lukas Weil <hl.weil@t-online.de>
2024-10-15 13:23:31 +0200

    include ARCtrl Common props in package for downstream fable transpilation

--- build/TestTasks.fs
+++ build/TestTasks.fs
@@ -9,6 +9,8 @@ open Fake.Core
 
 module RunTests = 
 
+    let skipTestsFlag = "--skipTests"
+
     let runTestsUI = BuildTask.create "runTestsUI" [clean; build] {
         let path = "tests/UI"
         Trace.traceImportant "Start UI tests"
@@ -18,46 +20,65 @@ module RunTests =
         run npx $"cypress run --component -P {path}" ""
     }
 
-    let runTestsJsNative = BuildTask.create "runTestsJSNative" [clean; build] {
-        Trace.traceImportant "Start native JavaScript tests"
-        for path in ProjectInfo.jsTestProjects do
-            // transpile library for native access
-            run dotnet $"fable src/ARCtrl/ARCtrl.Javascript.fsproj -o {path}/ARCtrl" ""
-            GenerateIndexJs.ARCtrl_generate($"{path}/ARCtrl")
-            run npx $"mocha {path} --timeout 20000" "" 
-    }
+    let runTestsJsNative = BuildTask.createFn "runTestsJSNative" [clean; build] (fun tp ->
+        if tp.Context.Arguments |> List.exists (fun a -> a.ToLower() = skipTestsFlag.ToLower()) |> not then
+            Trace.traceImportant "Start native JavaScript tests"
+            for path in ProjectInfo.jsTestProjects do
+                // transpile library for native access
+                run dotnet $"fable src/ARCtrl/ARCtrl.Javascript.fsproj -o {path}/ARCtrl" ""
+                GenerateIndexJs.ARCtrl_generate($"{path}/ARCtrl")
+                run npx $"mocha {path} --timeout 20000" ""
+        else
+            Trace.traceImportant "Skipping JavaScript tests"
+    )
 
-    let runTestsJs = BuildTask.create "runTestsJS" [clean; build] {
-        for path in ProjectInfo.testProjects do
-            // transpile js files from fsharp code
-            run dotnet $"fable {path} -o {path}/js" ""
-            // run mocha in target path to execute tests
-            // "--timeout 20000" is used, because json schema validation takes a bit of time.
-            run node $"{path}/js/Main.js" ""
-    }
+    let runTestsJs = BuildTask.createFn "runTestsJS" [clean; build] (fun tp ->
+        if tp.Context.Arguments |> List.exists (fun a -> a.ToLower() = skipTestsFlag.ToLower()) |> not then
+            Trace.traceImportant "Start Js tests"
+            for path in ProjectInfo.testProjects do
+                // transpile js files from fsharp code
+                run dotnet $"fable {path} -o {path}/js" ""
+                // run mocha in target path to execute tests
+                // "--timeout 20000" is used, because json schema validation takes a bit of time.
+                run node $"{path}/js/Main.js" ""
+        else
+            Trace.traceImportant "Skipping Js tests"
+    )
 
-    let runTestsPyNative = BuildTask.create "runTestsPyNative" [clean; build] {
-        Trace.traceImportant "Start native Python tests"
-        for path in ProjectInfo.pyTestProjects do
-            // transpile library for native access
-            run dotnet $"fable src/ARCtrl/ARCtrl.Python.fsproj -o {path}/ARCtrl --lang python" ""
-            GenerateIndexPy.ARCtrl_generate($"{path}/ARCtrl")
-            run python $"-m pytest {path}" "" 
-    }
+    let runTestsPyNative = BuildTask.createFn "runTestsPyNative" [clean; build] (fun tp ->
+        if tp.Context.Arguments |> List.exists (fun a -> a.ToLower() = skipTestsFlag.ToLower()) |> not then
+            Trace.traceImportant "Start native Python tests"
+            for path in ProjectInfo.pyTestProjects do
+                // transpile library for native access
+                run dotnet $"fable src/ARCtrl/ARCtrl.Python.fsproj -o {path}/ARCtrl --lang python" ""
+                GenerateIndexPy.ARCtrl_generate($"{path}/ARCtrl")
+                run python $"-m pytest {path}" ""
+        else
+            Trace.traceImportant "Skipping Python tests"
+    )
 
-    let runTestsPy = BuildTask.create "runTestsPy" [clean; build] {
-        for path in ProjectInfo.testProjects do
-            //transpile py files from fsharp code
-            run dotnet $"fable {path} -o {path}/py --lang python" ""
-            // run pyxpecto in target path to execute tests in python
-            run python $"{path}/py/main.py" ""
-    }
+    let runTestsPy = BuildTask.createFn "runTestsPy" [clean; build] (fun tp ->
+        if tp.Context.Arguments |> List.exists (fun a -> a.ToLower() = skipTestsFlag.ToLower()) |> not then
+            Trace.traceImportant "Start Python tests"
+            for path in ProjectInfo.testProjects do
+                //transpile py files from fsharp code
+                run dotnet $"fable {path} -o {path}/py --lang python" ""
+                // run pyxpecto in target path to execute tests in python
+                run python $"{path}/py/main.py" ""
+        else
+            Trace.traceImportant "Skipping Python tests"
 
-    let runTestsDotnet = BuildTask.create "runTestsDotnet" [clean; build] {
-        let dotnetRun = run dotnet "run"
-        testProjects
-        |> Seq.iter dotnetRun
-    }
+    )
+
+    let runTestsDotnet = BuildTask.createFn "runTestsDotnet" [clean; build] (fun tp ->
+        if tp.Context.Arguments |> List.exists (fun a -> a.ToLower() = skipTestsFlag.ToLower()) |> not then
+            Trace.traceImportant "Start .NET tests"
+            let dotnetRun = run dotnet "run"
+            testProjects
+            |> Seq.iter dotnetRun
+        else
+            Trace.traceImportant "Skipping .NET tests"
+    )
 
     let runTestProject = BuildTask.createFn "runTestProject" [clean; build] (fun config ->
         let dotnetRun = run dotnet "run"
