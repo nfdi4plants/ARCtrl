98b1b991da14d4cfd6214edc2fd04358267378a3
Heinrich Lukas Weil <hl.weil@t-online.de>
2024-10-29 15:07:04 +0100

    add testObjects from ARCtrl.IO

---
+++ tests/TestingUtils/TestObjects.IO.fs
@@ -0,0 +1,4 @@
+module TestObjects.IO
+
+let testInputFolder = System.IO.Path.Combine(__SOURCE_DIRECTORY__,@"TestObjects/Contracts")
+let testOutputFolder = System.IO.Path.Combine(__SOURCE_DIRECTORY__,@"TestResults/Contracts")
