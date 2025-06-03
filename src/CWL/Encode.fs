f613af8bb1480997d8175b7b8e3e02a53158e809
HLWeil <hl.weil@t-online.de>
2025-06-03 10:04:02 +0200

    rename CWL Description to ProcessingUnit and add parsers

--- src/CWL/Encode.fs
+++ src/CWL/Encode.fs
@@ -1,1 +1,16 @@
 namespace ARCtrl.CWL
+
+module Encode =
+
+    /// Decode a CWL file string written in the YAML format into a CWLToolDescription
+    let encodeCommandLineTool (clt : CWLToolDescription) : string =
+        failwith "Encoding of CommandLineTool is not implemented yet"
+
+    /// Decode a CWL file string written in the YAML format into a CWLWorkflowDescription
+    let encodeWorkflow (wf : CWLWorkflowDescription) : string =
+        failwith "Encoding of Workflow is not implemented yet"
+
+    let encodeCWLProcessingUnit (cwl : CWLProcessingUnit) : string =
+        match cwl with
+        | CommandLineTool clt -> encodeCommandLineTool clt
+        | Workflow wf -> encodeWorkflow wf
