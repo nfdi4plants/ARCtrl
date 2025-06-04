27d9ed96544949cc4232f8edfad1d7cb817e6e77
Caroline Ott <caroline.ott1994@gmail.com>
2025-06-04 15:52:56 +0200

    fix tests for dotnet sequences

--- tests/CWL/YAMLParameterFile.Tests.fs
+++ tests/CWL/YAMLParameterFile.Tests.fs
@@ -13,50 +13,55 @@ let testYAMLParameterFile =
     testList "Decode" [
         testCase "Length" <| fun _ -> Expect.equal 5  decodeYAMLParameterFile.Count ""
         testCase "exampleKeyInt" <| fun _ ->
-            Expect.equal
+            let expected =
                 {
                     Key = "exampleKey"
                     Values = ResizeArray [| "1234" |]
                     Type = None
                 }
-                decodeYAMLParameterFile.[0]
-                ""
+            Expect.equal expected.Key decodeYAMLParameterFile.[0].Key ""
+            Expect.sequenceEqual expected.Values decodeYAMLParameterFile.[0].Values ""
+            Expect.equal expected.Type decodeYAMLParameterFile.[0].Type ""
         testCase "exampleKeyString" <| fun _ ->
-            Expect.equal
+            let expected =
                 {
                     Key = "exampleKeyString"
                     Values = ResizeArray [| "abcdefg" |]
                     Type = None
                 }
-                decodeYAMLParameterFile.[1]
-                ""
+            Expect.equal expected.Key decodeYAMLParameterFile.[1].Key ""
+            Expect.sequenceEqual expected.Values decodeYAMLParameterFile.[1].Values ""
+            Expect.equal expected.Type decodeYAMLParameterFile.[1].Type ""
         testCase "exampleFile" <| fun _ ->
-            Expect.equal
+            let expected =
                 {
                     Key = "exampleFile"
                     Values = ResizeArray [| "../examplePath" |]
                     Type = Some "File"
                 }
-                decodeYAMLParameterFile.[2]
-                ""
+            Expect.equal expected.Key decodeYAMLParameterFile.[2].Key ""
+            Expect.sequenceEqual expected.Values decodeYAMLParameterFile.[2].Values ""
+            Expect.equal expected.Type decodeYAMLParameterFile.[2].Type ""
         testCase "exampleDir" <| fun _ ->
-            Expect.equal
+            let expected =
                 {
                     Key = "exampleDir"
                     Values = ResizeArray [| "../examplePathDir" |]
                     Type = Some "Directory"
                 }
-                decodeYAMLParameterFile.[3]
-                ""
+            Expect.equal expected.Key decodeYAMLParameterFile.[3].Key ""
+            Expect.sequenceEqual expected.Values decodeYAMLParameterFile.[3].Values ""
+            Expect.equal expected.Type decodeYAMLParameterFile.[3].Type ""
         testCase "exampleList" <| fun _ ->
-            Expect.equal
+            let expected =
                 {
                     Key = "exampleList"
                     Values = ResizeArray [| "foo.txt"; "bar.dat"; "baz.txt" |]
                     Type = None
                 }
-                decodeYAMLParameterFile.[4]
-                ""
+            Expect.equal expected.Key decodeYAMLParameterFile.[4].Key ""
+            Expect.sequenceEqual expected.Values decodeYAMLParameterFile.[4].Values ""
+            Expect.equal expected.Type decodeYAMLParameterFile.[4].Type ""
     ]
 
 let main = 
