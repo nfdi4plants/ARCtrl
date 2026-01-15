841914d07b3aea768d73396f930077c008832f18
caroott <caroline.ott1994@gmail.com>
2026-01-15 10:34:41 +0100

    add custom equaltiy to cwltype and mutually recursive types

--- tests/CWL/Inputs.Tests.fs
+++ tests/CWL/Inputs.Tests.fs
@@ -28,33 +28,16 @@ let testInput =
             let fileItem = decodeInput.[2]
             testCase "Name" <| fun _ -> Expect.equal "argOptional"  fileItem.Name ""
             testCase "Type" <| fun _ -> 
-                Expect.isTrue (
-                    match fileItem.Type_.Value with
-                    | Union types ->
-                        types.Count = 2 &&
-                        types.[0] = Null &&
-                        (match types.[1] with File _ -> true | _ -> false)
-                    | _ -> false
-                ) "Should be Union of [Null; File]"
+                let expected = Union (ResizeArray [Null; File (FileInstance())])
+                Expect.equal fileItem.Type_.Value expected "Should be Union of [Null; File]"
             testCase "Optional" <| fun _ -> Expect.equal (Some true) fileItem.Optional ""
         ]
         testList "File array optional" [
             let fileItem = decodeInput.[3]
             testCase "Name" <| fun _ -> Expect.equal "argOptionalMap"  fileItem.Name ""
             testCase "Type" <| fun _ -> 
-                Expect.isTrue (
-                    match fileItem.Type_.Value with
-                    | Union types ->
-                        types.Count = 2 &&
-                        types.[0] = Null &&
-                        (match types.[1] with
-                         | Array arraySchema ->
-                             match arraySchema.Items with
-                             | File _ -> true
-                             | _ -> false
-                         | _ -> false)
-                    | _ -> false
-                ) "Should be Union of [Null; File[]]"
+                let expected = Union (ResizeArray [Null; Array {Items = File (FileInstance()); Label = None; Doc = None; Name = None}])
+                Expect.equal fileItem.Type_.Value expected "Should be Union of [Null; File[]]"
             testCase "Optional" <| fun _ -> Expect.equal (Some true) fileItem.Optional ""
         ]
         testList "String" [
