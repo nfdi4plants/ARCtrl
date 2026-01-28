018e167c76893a07ee261982c5c3c4076ace55a4
simonfoellinger <simonfoellinger@web.de>
2026-01-28 04:23:41 +0100

    Add mapping and JSON requirements decoding
    Extend Decode.requirementArrayDecoder to handle requirements expressed as mapping syntax and JSON flow mappings, in addition to array syntax.
    Update Requirements.Tests to cover class syntax, mapping syntax, and JSON syntax inputs.

--- tests/TestingUtils/TestObjects.CWL/Requirements.fs
+++ tests/TestingUtils/TestObjects.CWL/Requirements.fs
@@ -2,7 +2,7 @@ module TestObjects.CWL.Requirements
 
 open ARCtrl.CWL
 
-let requirementsFileContent ="""requirements:
+let requirementsClassFileContent ="""requirements:
   - class: DockerRequirement
     dockerImageId: "devcontainer"
     dockerFile: {$include: "FSharpArcCapsule/Dockerfile"}
@@ -26,6 +26,75 @@ let requirementsFileContent ="""requirements:
         version: [ "5.21-60" ]
   - class: NetworkAccess
     networkAccess: true"""
+
+let requirementsMappingFileContent ="""requirements:
+  DockerRequirement:
+    dockerImageId: "devcontainer"
+    dockerFile: {$include: "FSharpArcCapsule/Dockerfile"}
+  InitialWorkDirRequirement:
+    listing:
+      - entryname: arc
+        entry: $(inputs.arcDirectory)
+        writable: true
+      - entry: $(inputs.outputDirectory)
+        writable: true
+  EnvVarRequirement:
+    envDef:
+      - envName: DOTNET_NOLOGO
+        envValue: "true"
+      - envName: TEST
+        envValue: "false"
+  SoftwareRequirement:
+    packages:
+      - package: interproscan
+        specs: [ "https://identifiers.org/rrid/RRID:SCR_005829" ]
+        version: [ "5.21-60" ]
+  NetworkAccess:
+    networkAccess: true"""
+
+let requirementsJSONFileContent ="""
+requirements: {
+  DockerRequirement: {
+    dockerImageId: "devcontainer",
+    dockerFile: { $include: "FSharpArcCapsule/Dockerfile" }
+  },
+  InitialWorkDirRequirement: {
+    listing: [
+      { entryname: "arc", entry: "$(inputs.arcDirectory)", writable: true },
+      { entry: "$(inputs.outputDirectory)", writable: true }
+    ]
+  },
+  EnvVarRequirement: {
+    envDef: [
+      { envName: "DOTNET_NOLOGO", envValue: "true" },
+      { envName: "TEST", envValue: "false" }
+    ]
+  },
+  SoftwareRequirement: {
+    packages: [
+      {
+        package: "interproscan",
+        specs: [ "https://identifiers.org/rrid/RRID:SCR_005829" ],
+        version: [ "5.21-60" ]
+      }
+    ]
+  },
+  NetworkAccess: { networkAccess: true }
+}
+"""
+
+let requirementsArraySyntax = """
+requirements:
+  - class: SubworkflowFeatureRequirement
+"""
+let requirementsMappingSyntax = """
+requirements:
+  SubworkflowFeatureRequirement: {}
+"""
+
+let requirementsJSONSyntax = """
+requirements: { SubworkflowFeatureRequirement: {} }
+"""
 
 module Docker =
 
