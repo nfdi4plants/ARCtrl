08d877e1a795c87d8a8b8da118704fe22ee7c30c
caroott <caroline.ott1994@gmail.com>
2026-02-11 11:18:29 +0100

    add missing fields

--- src/CWL/WorkflowSteps.fs
+++ src/CWL/WorkflowSteps.fs
@@ -2,6 +2,7 @@ namespace ARCtrl.CWL
 
 open DynamicObj
 open Fable.Core
+open YAMLicious.YAMLiciousTypes
 
 [<AttachMembers>]
 type LinkMergeMethod =
@@ -20,12 +21,52 @@ type LinkMergeMethod =
         | _ -> None
 
 [<AttachMembers>]
+type PickValueMethod =
+    | FirstNonNull
+    | TheOnlyNonNull
+    | AllNonNull
+    with
+    member this.AsCwlString =
+        match this with
+        | FirstNonNull -> "first_non_null"
+        | TheOnlyNonNull -> "the_only_non_null"
+        | AllNonNull -> "all_non_null"
+
+    static member tryParse(value: string) =
+        match value with
+        | "first_non_null" -> Some FirstNonNull
+        | "the_only_non_null" -> Some TheOnlyNonNull
+        | "all_non_null" -> Some AllNonNull
+        | _ -> None
+
+[<AttachMembers>]
+type ScatterMethod =
+    | DotProduct
+    | NestedCrossProduct
+    | FlatCrossProduct
+    with
+    member this.AsCwlString =
+        match this with
+        | DotProduct -> "dotproduct"
+        | NestedCrossProduct -> "nested_crossproduct"
+        | FlatCrossProduct -> "flat_crossproduct"
+
+    static member tryParse(value: string) =
+        match value with
+        | "dotproduct" -> Some DotProduct
+        | "nested_crossproduct" -> Some NestedCrossProduct
+        | "flat_crossproduct" -> Some FlatCrossProduct
+        | _ -> None
+
+[<AttachMembers>]
 type StepInput = {
     Id: string
     Source: ResizeArray<string> option
-    DefaultValue: string option
+    DefaultValue: YAMLElement option
     ValueFrom: string option
     LinkMerge: LinkMergeMethod option
+    PickValue: PickValueMethod option
+    Doc: string option
     LoadContents: bool option
     LoadListing: string option
     Label: string option
@@ -35,9 +76,11 @@ type StepInput = {
     static member create(
         id: string,
         ?source: ResizeArray<string>,
-        ?defaultValue: string,
+        ?defaultValue: YAMLElement,
         ?valueFrom: string,
         ?linkMerge: LinkMergeMethod,
+        ?pickValue: PickValueMethod,
+        ?doc: string,
         ?loadContents: bool,
         ?loadListing: string,
         ?label: string
@@ -47,17 +90,19 @@ type StepInput = {
           DefaultValue = defaultValue
           ValueFrom = valueFrom
           LinkMerge = linkMerge
+          PickValue = pickValue
+          Doc = doc
           LoadContents = loadContents
           LoadListing = loadListing
           Label = label }
 
 [<AttachMembers>]
 type StepOutputParameter = {
-    Id: string option
+    Id: string
 }
 
     with
-    static member create(?id: string) =
+    static member create(id: string) =
         { Id = id }
 
 [<AttachMembers>]
@@ -80,7 +125,8 @@ type WorkflowStep (
     ?label: string,
     ?doc: string,
     ?scatter: ResizeArray<string>,
-    ?scatterMethod: string,
+    ?scatterMethod: ScatterMethod,
+    ?when_: string,
     ?requirements: ResizeArray<Requirement>,
     ?hints: ResizeArray<Requirement>
 ) =
@@ -93,7 +139,8 @@ type WorkflowStep (
     let mutable _label: string option = label
     let mutable _doc: string option = doc
     let mutable _scatter: ResizeArray<string> option = scatter
-    let mutable _scatterMethod: string option = scatterMethod
+    let mutable _scatterMethod: ScatterMethod option = scatterMethod
+    let mutable _when: string option = when_
     let mutable _requirements: ResizeArray<Requirement> option = requirements
     let mutable _hints: ResizeArray<Requirement> option = hints
 
@@ -105,7 +152,8 @@ type WorkflowStep (
         ?label: string,
         ?doc: string,
         ?scatter: ResizeArray<string>,
-        ?scatterMethod: string,
+        ?scatterMethod: ScatterMethod,
+        ?when_: string,
         ?requirements: ResizeArray<Requirement>,
         ?hints: ResizeArray<Requirement>
     ) =
@@ -118,6 +166,7 @@ type WorkflowStep (
             ?doc = doc,
             ?scatter = scatter,
             ?scatterMethod = scatterMethod,
+            ?when_ = when_,
             ?requirements = requirements,
             ?hints = hints
         )
@@ -153,6 +202,10 @@ type WorkflowStep (
     member this.ScatterMethod
         with get() = _scatterMethod
         and set(scatterMethod) = _scatterMethod <- scatterMethod
+
+    member this.When_
+        with get() = _when
+        and set(when_) = _when <- when_
 
     member this.Requirements
         with get() = _requirements
