ece2aa99fc67203cd173f2220651582f9e8fc2ab
caroott <caroline.ott1994@gmail.com>
2026-02-11 11:19:43 +0100

    add wfsteps outside for safer type implementation

--- src/CWL/CWLProcessingUnit.fs
+++ src/CWL/CWLProcessingUnit.fs
@@ -3,3 +3,22 @@ namespace ARCtrl.CWL
 type CWLProcessingUnit =
     | CommandLineTool of CWLToolDescription
     | Workflow of CWLWorkflowDescription
+
+[<RequireQualifiedAccess>]
+module WorkflowStepRunOps =
+
+    let fromTool (tool: CWLToolDescription) : WorkflowStepRun =
+        RunCommandLineTool tool
+
+    let fromWorkflow (workflow: CWLWorkflowDescription) : WorkflowStepRun =
+        RunWorkflow workflow
+
+    let tryGetTool (run: WorkflowStepRun) : CWLToolDescription option =
+        match run with
+        | RunCommandLineTool (:? CWLToolDescription as tool) -> Some tool
+        | _ -> None
+
+    let tryGetWorkflow (run: WorkflowStepRun) : CWLWorkflowDescription option =
+        match run with
+        | RunWorkflow (:? CWLWorkflowDescription as workflow) -> Some workflow
+        | _ -> None
